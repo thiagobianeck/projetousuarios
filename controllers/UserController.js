aac7b959e81f18b11ec79edfe64176056ecdeb5f
Thiago Bianeck <thiagobianeck@gmail.com>
2018-12-17 20:28:01 -0200

    G12 - O operador Spread ...

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -17,7 +17,7 @@ class UserController {
     getValues() {
 
         let user = {};
-        Object.values(this.formEl.elements).forEach(function (field, index) {
+        [...this.formEl.elements].forEach(function (field, index) {
             if (field.name == 'gender') {
                 if (field.checked) {
                     user[field.name] = field.value;
