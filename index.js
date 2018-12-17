0e625a72905f8e60438f8f411707a7d2985f0c80
Thiago Bianeck <thiagobianeck@gmail.com>
2018-12-17 00:51:04 -0200

    G7 - Trabalhando com Json

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-var name = document.querySelector('#exampleInputName');
-var gender = document.querySelectorAll('#form-user-create [name=gender]:checked')
-var birth = document.querySelector('#exampleInputBirth');
-var country = document.querySelector('#exampleInputCountry');
-var email = document.querySelector('#exampleInputEmail');
-var password = document.querySelector('#exampleInputPassword');
-var photo = document.querySelector('#exampleInputFile');
-var admin = document.querySelector('#exampleInputAdmin');
-
-
-
 var fields = document.querySelectorAll('#form-user-create [name]');
+
+var user = {};
 
 fields.forEach(function (field, index) {
     if (field.name == 'gender') {
         if (field.checked) {
-
+            user[field.name] = field.value;
         }
     } else {
-
+        user[field.name] = field.value;
     }
 });
