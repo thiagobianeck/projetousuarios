37545664b065a0b889073666a6742532912374bb
Thiago Bianeck <thiagobianeck@gmail.com>
2018-12-20 10:46:33 -0200

    G18 - Validando Formulários

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -64,7 +64,15 @@ class UserController {
     getValues() {
 
         let user = {};
+        let isValid = true;
+
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if (['name','email','password'].indexOf(field.name) > -1 && !field.value) {
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
+
             if (field.name == 'gender') {
                 if (field.checked) {
                     user[field.name] = field.value;
@@ -75,6 +83,10 @@ class UserController {
                 user[field.name] = field.value;
             }
         });
+
+        if(!isValid) {
+            return false;
+        }
 
         return new User(
             user.name,
