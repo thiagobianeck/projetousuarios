d4f9afa67e81279178e25150fc9b8930f2d58c27
Thiago Bianeck <thiagobianeck@gmail.com>
2018-12-20 00:23:40 -0200

    G16 - Trabalhando com Datas, Getters e Setters

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,12 +11,17 @@ class UserController {
         this.formEl.addEventListener('submit', event => {
             event.preventDefault();
 
+            let btn = this.formEl.querySelector('[type=submit]');
+            btn.disabled = true;
+
             let values = this.getValues();
 
             this.getPhoto().then(
                 content => {
                     values.photo = content;
                     this.addLine(values);
+                    this.formEl.reset();
+                    btn.disabled = false;
                 },
                 e => console.error(e)
 
@@ -94,7 +99,7 @@ class UserController {
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-                <td>${dataUser.birth}</td>
+                <td>${dataUser.register}</td>
                 <td>
                   <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                   <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
